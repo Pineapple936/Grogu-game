02e7d1ff2bb427f22849aee9e3383e9cef51ca8d
Pineapple936 <tarasovegor545@gmail.com>
2024-12-29 21:39:32 +0300

    add animation for Grogu

--- app/objects/hero.py
+++ app/objects/hero.py
@@ -2,7 +2,13 @@ import pygame
 
 class Hero:
     def __init__(self, widthScreen, heightScreen, groundY):
-        self.__image = pygame.transform.scale(pygame.image.load("image/grogu/grogu_start.png"), (widthScreen // 10, heightScreen // 10))
+        self.__images = {
+            "run": (
+                pygame.transform.scale(pygame.image.load("image/grogu/grogu_run_1.png"), (widthScreen // 10, heightScreen // 10)),
+                pygame.transform.scale(pygame.image.load("image/grogu/grogu_run_2.png"), (widthScreen // 10, heightScreen // 10))),
+            "jump": pygame.transform.scale(pygame.image.load("image/grogu/grogu_jump.png"), (widthScreen // 10, heightScreen // 10))
+        }
+        self.__image = self.__images["run"][0]
         self.__defaultY = groundY - self.__image.get_height()
         self.__rect = self.__image.get_rect()
         self.__rect.y = self.__defaultY
@@ -10,6 +16,7 @@ class Hero:
         self.__defaultJumpSpeed = heightScreen // 12
         self.__jumpSpeed = self.__defaultJumpSpeed
         self.__verticalVelocity = heightScreen // 120
+        self.__animCount = 0
 
     def __check_button(self, keys):
         if keys[pygame.K_SPACE] and not self.__jumpStatus:
@@ -29,6 +36,19 @@ class Hero:
         if self.__rect.y > self.__defaultY:
             self.__jumpStatus = False
             self.__rect.y = self.__defaultY
+        self.__switch_animation()
+
+    def __switch_animation(self):
+        if self.__jumpStatus:
+            self.__image = self.__images["jump"]
+        else:
+            self.__animCount += 1
+            if self.__animCount // 5 == 1 or self.__animCount // 5 == 2:
+                self.__image = self.__images["run"][self.__animCount // 5 - 1]
+
+            if self.__animCount == 10:
+                self.__animCount = 0
+
 
     def draw(self, screen):
         screen.blit(self.__image, self.__rect)
